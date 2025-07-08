997a17d81b3199cdd9646f9b18a53ee99b69d9dc
Debarshi De <de.debarshi@gmail.com>
2025-07-08 16:05:52 -0500

    use hook for params

--- src/app/api/single/[id]/route.js
+++ src/app/api/single/[id]/route.js
@@ -1,13 +1,14 @@
 import { NextResponse } from 'next/server';
 import dbConnect from '@/lib/dbConnect';
 import Confession from '@/models/confession';
+import { use } from 'react';
 
 const ObjectId = require('mongoose').Types.ObjectId;
 
 export async function GET(res, { params }) {
   await dbConnect();
 
-  const confessionID = params.id;
+  const confessionID = use(params).id;
 
   if (!ObjectId.isValid(confessionID)) {
     return NextResponse.json('No record with given id : ', confessionID);
