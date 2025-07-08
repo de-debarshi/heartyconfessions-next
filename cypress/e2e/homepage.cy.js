eb0f1c66c8ee019d14f86db6b8ac51e97946453f
Debarshi De <de.debarshi@gmail.com>
2025-07-08 16:59:59 -0500

    setup husky pre-commit hook

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,19 +1,19 @@
 describe('Homepage', () => {
-    it('should load the homepage and show title', () => {
-        cy.visit('/');
+  it('should load the homepage and show title', () => {
+    cy.visit('/');
 
-        // Check page title
-        cy.title().should('include', 'Hearty Confessions');
+    // Check page title
+    cy.title().should('include', 'Hearty Confessions');
 
-        // Check heading or any visible element
-        cy.contains('Explore Stories').should('be.visible');
+    // Check heading or any visible element
+    cy.contains('Explore Stories').should('be.visible');
 
-        cy.contains('Explore Stories').click(); // clicks link or button with "Explore" text
+    cy.contains('Explore Stories').click(); // clicks link or button with "Explore" text
 
-        // Wait for navigation to complete
-        cy.url().should('include', '/explore');
+    // Wait for navigation to complete
+    cy.url().should('include', '/explore');
 
-        // Then assert something on that page
-        cy.contains('Scroll down to explore some interesting stories!').should('be.visible');
-    });
+    // Then assert something on that page
+    cy.contains('Scroll down to explore some interesting stories!').should('be.visible');
+  });
 });
--- src/app/api/like/[id]/route.js
+++ src/app/api/like/[id]/route.js
@@ -7,8 +7,8 @@ const ObjectId = require('mongoose').Types.ObjectId;
 
 export async function PUT(req, { params }) {
   await dbConnect();
-
-  const confessionID = use(params).id;
+  const { id } = await params;
+  const confessionID = id;
 
   if (!ObjectId.isValid(confessionID)) {
     return NextResponse.json('No record with given id : ', confessionID);
